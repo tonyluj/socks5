f421e24a3e9793ddebff19326b4d3da79bc3a08a
Tony Lu <tonyluj@gmail.com>
2017-09-17 12:47:09 +0800

    Accept error retry
    Signed-off-by: Tony Lu <tonyluj@gmail.com>

--- socks5.go
+++ socks5.go
@@ -3,6 +3,7 @@ package socks5
 import (
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net"
@@ -13,10 +14,11 @@ import (
 )
 
 var (
-	errAddrType = errors.New("unsupport address type")
-	errVersion  = errors.New("unsupport socks version")
-	errMethod   = errors.New("unsupport method")
-	errCmd      = errors.New("unsupport command")
+	errExtraData = errors.New("get extra data")
+	errAddrType  = errors.New("unsupport address type")
+	errVersion   = errors.New("unsupport socks version")
+	errMethod    = errors.New("unsupport method")
+	errCmd       = errors.New("unsupport command")
 )
 
 const (
@@ -24,6 +26,7 @@ const (
 
 	methodNoAuth           = 0x00
 	methodUsernamePassword = 0x02
+	methodNoAcceptable     = 0xff
 
 	version5 = 0x05
 
@@ -78,10 +81,25 @@ func (s *Server) Listen() (err error) {
 		return
 	}
 
+	var tempDelay time.Duration
 	for {
 		conn, err := ln.AcceptTCP()
 		if err != nil {
-			continue
+			if ne, ok := err.(net.Error); ok && ne.Temporary() {
+				if tempDelay == 0 {
+					tempDelay = 5 * time.Millisecond
+				} else {
+					tempDelay *= 2
+				}
+
+				if max := 1 * time.Second; tempDelay > max {
+					tempDelay = max
+				}
+
+				time.Sleep(tempDelay)
+				continue
+			}
+			return err
 		}
 
 		go s.handle(conn)
@@ -89,8 +107,12 @@ func (s *Server) Listen() (err error) {
 }
 
 func (s *Server) handle(conn net.Conn) {
-	defer conn.Close()
-	conn.SetDeadline(time.Now().Add(s.timeout))
+	//defer conn.Close()
+	defer func() {
+		fmt.Println("close conn", conn.RemoteAddr())
+		conn.Close()
+	}()
+	//conn.SetReadDeadline(time.Now())
 
 	// check version and nmethod
 	err := s.handShake(conn)
@@ -117,11 +139,14 @@ func (s *Server) handle(conn net.Conn) {
 			s.logger.Println("reply", err)
 			return
 		}
-
 		return
 	}
-	defer rconn.Close()
-	rconn.SetDeadline(time.Now().Add(s.timeout))
+	//defer rconn.Close()
+	defer func() {
+		fmt.Println("close rconn", rconn.RemoteAddr())
+		rconn.Close()
+	}()
+	//rconn.SetDeadline(time.Now().Add(s.timeout))
 
 	// reply success
 	err = s.reply(conn, repSucceed)
@@ -140,48 +165,55 @@ func (s *Server) handle(conn net.Conn) {
 
 // handShake is used for check version
 func (s *Server) handShake(conn net.Conn) (err error) {
-	//b := s.pool.Get().([]byte)
-	//defer s.pool.Put(b)
 	// version 1, nmethod 1, methods 255
-	//buf := b[:257]
 	buf := s.pool.Get().([]byte)
 	defer s.pool.Put(buf)
 
 	// read at least 2 bytes (version and auth method)
-	_, err = io.ReadAtLeast(conn, buf, 2)
+	n, err := io.ReadAtLeast(conn, buf, 2)
 	if err != nil {
 		return
 	}
+
 	// check socks version
 	if buf[0] != version5 {
 		err = errVersion
 		return
 	}
 
-	// check method
-	switch buf[1] {
-	case methodNoAuth:
+	ml := int(buf[1]) + 2
+	switch {
+	case n == ml: // already read all
+	case n < ml: // no more
+		_, err = io.ReadFull(conn, buf[n:ml])
+		if err != nil {
+			return
+		}
 	default:
-		//err = errMethod
-		//return
+		err = errExtraData
+		return
 	}
 
+	// TODO: check method no auth method list
+
+	// TODO: reuse []byte
 	// only support NO AUTH method
-	_, err = conn.Write([]byte{version5, methodNoAuth})
+	buf[1] = methodNoAuth
+	_, err = conn.Write(buf[:2])
+	if err != nil {
+		return
+	}
 	return
 }
 
 // handle request, get address
 func (s *Server) request(conn net.Conn) (host string, err error) {
-	//b := s.pool.Get().([]byte)
-	//defer s.pool.Put(b)
 	// version 1, cmd 1, rsv 1, atyp 1, addr max domain length is 253, port 2
-	//buf := b[:258]
 	buf := s.pool.Get().([]byte)
 	defer s.pool.Put(buf)
 
 	// read at least fetch ipv4
-	_, err = io.ReadAtLeast(conn, buf, 10)
+	n, err := io.ReadAtLeast(conn, buf, 10)
 	if err != nil {
 		return
 	}
@@ -204,22 +236,23 @@ func (s *Server) request(conn net.Conn) (host string, err error) {
 		dstPort int
 	)
 	switch buf[3] {
-	case atypIPV4:
+	case atypIPV4: // alread read all
+		// TODO: BigEndian
 		// ipv4 start from 4, and ipv4 length is 4, port start from 8, total length is 10
 		dstAddr = net.IP(buf[4 : 4+4]).String()
 		dstPort = int(binary.BigEndian.Uint16(buf[8:10]))
-	case atypIPV6:
-		_, err = io.ReadFull(conn, buf[10:22])
+	case atypIPV6: // read rest
+		_, err = io.ReadFull(conn, buf[n:22])
 		if err != nil {
 			return
 		}
 		// ipv6 start from 4, and ipv6 lenght is 16, port start from 20, total length is 22
 		dstAddr = net.IP(buf[4 : 4+16]).String()
 		dstPort = int(binary.BigEndian.Uint16(buf[20:22]))
-	case atypDomain:
+	case atypDomain: // read rest
 		// domain length is 4, start from 5, total length is domain length + 7
 		dstAddrLen := buf[4]
-		_, err = io.ReadFull(conn, buf[10:dstAddrLen+7])
+		_, err = io.ReadFull(conn, buf[n:dstAddrLen+7])
 		if err != nil {
 			return
 		}
@@ -230,20 +263,21 @@ func (s *Server) request(conn net.Conn) (host string, err error) {
 		return
 	}
 	host = net.JoinHostPort(dstAddr, strconv.Itoa(dstPort))
-
 	return
 }
 
 // TODO
 func (s *Server) reply(conn net.Conn, rep byte) (err error) {
 	// version 1, rep 1, rsv 1, atyp 1, addr 4 or 16(ipv4 or ipv6), port 2
-	rb := make([]byte, 10)
+	buf := s.pool.Get().([]byte)
+	defer s.pool.Put(buf)
+
 	// TODO: support ipv6
-	copy(rb[0:4], []byte{version5, rep, rsv, atypIPV4})
+	copy(buf[0:4], []byte{version5, rep, rsv, atypIPV4})
 	// TODO ip empty
-	copy(rb[4:8], []byte{0x00, 0x00, 0x00, 0x00})
-	binary.BigEndian.PutUint16(rb[8:10], uint16(s.addr.Port))
-	_, err = conn.Write(rb)
+	copy(buf[4:8], []byte{0x00, 0x00, 0x00, 0x00})
+	binary.BigEndian.PutUint16(buf[8:10], uint16(s.addr.Port))
+	_, err = conn.Write(buf[:10])
 	if err != nil {
 		return
 	}
@@ -251,62 +285,44 @@ func (s *Server) reply(conn net.Conn, rep byte) (err error) {
 }
 
 // connect transfer data between src and remote
-// TODO better implement?
+// 1. client open, remote open: if oocur any error, close both
+// 2. client close, remote open: close remote
+// 3. client open, remote close: read remote data and write to client, then close client
+// 4. client close, remote close: rare condition
 func (s *Server) connect(dst, src net.Conn) (err error) {
-	//errChan := make(chan error, 1)
 	errChan := make(chan error, 2)
-	wg := sync.WaitGroup{}
-	wg.Add(2)
 	// read from src, write to remote
 	go func() {
-		defer func() {
-			wg.Done()
-		}()
-
-		// get buffer from pool
-		b := s.pool.Get().([]byte)
-		_, err := s.copy(dst, src, b)
-		// send back buffer to pool
-		s.pool.Put(b)
-		if err != nil {
-			errChan <- err
-			return
-		}
+		_, err := s.copy(dst, src)
+		errChan <- err
 	}()
 
 	// read from remote, write to src
 	go func() {
-		defer func() {
-			wg.Done()
-		}()
-
-		// get buffer from pool
-		b := s.pool.Get().([]byte)
-		_, err := s.copy(src, dst, b)
-		// send back buffer to pool
-		s.pool.Put(b)
-		if err != nil {
-			errChan <- err
-			return
-		}
+		_, err := s.copy(src, dst)
+		errChan <- err
 	}()
 
-	wg.Wait()
-	close(errChan)
-	err = <-errChan
+	select {
+	case err = <-errChan:
+	}
 	return
 }
 
 // copy data from src to dst
-func (s *Server) copy(dst, src net.Conn, buf []byte) (wriiten int64, err error) {
+func (s *Server) copy(dst, src net.Conn) (wriiten int64, err error) {
+	buf := s.pool.Get().([]byte)
+	defer s.pool.Put(buf)
+
 	for {
+		// src will timeout
 		nr, er := src.Read(buf)
 		if nr > 0 {
 			nw, ew := dst.Write(buf[:nr])
 			if nw > 0 {
 				wriiten += int64(nw)
 			}
-			if err != nil {
+			if ew != nil {
 				err = ew
 				break
 			}
@@ -315,14 +331,8 @@ func (s *Server) copy(dst, src net.Conn, buf []byte) (wriiten int64, err error) 
 				break
 			}
 		}
-		if er == io.EOF {
-			// if recv EOF from src, write EOF to remote too
-			// TODO
-			dst.Write([]byte{0})
-			break
-		}
 		if er != nil {
-			err = er
+			//err = er
 			break
 		}
 	}
